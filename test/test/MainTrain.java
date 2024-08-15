6b88b8a977019b61f484cb82f172875155738811
Ofek Segman <ofeksegman@gmail.com>
2024-08-15 10:46:51 +0300

    tile and bag word

--- test/test/MainTrain.java
+++ test/test/MainTrain.java
@@ -1,50 +1,53 @@
 package test;
- //import test.Tile.Bag;
+
+import test.Tile.Bag;
+
+//import test.Tile.Bag;
  public class MainTrain {
-// 	public static void testBag() {
-// 		Bag b=Tile.Bag.getBag();
-// 		Bag b1=Tile.Bag.getBag();
-// 		if(b1!=b)
-// 			System.out.println("your Bag in not a Singleton (-5)");
+ 	public static void testBag() {
+ 		Bag b=Tile.Bag.getBag();
+ 		Bag b1=Tile.Bag.getBag();
+ 		if(b1!=b)
+ 			System.out.println("your Bag in not a Singleton (-5)");
 		
-// 		int[] q0 = b.getQuantities();
-// 		q0[0]+=1;
-// 		int[] q1 = b.getQuantities();
-// 		if(q0[0]!=q1[0] + 1)
-// 			System.out.println("getQuantities did not return a clone (-5)");
+ 		int[] q0 = b.getQuantities();
+ 		q0[0]+=1;
+ 		int[] q1 = b.getQuantities();
+ 		if(q0[0]!=q1[0] + 1)
+ 			System.out.println("getQuantities did not return a clone (-5)");
 		
-// 		for(int k=0;k<9;k++) {
-// 			int[] qs = b.getQuantities(); 
-// 			Tile t = b.getRand();
-// 			int i=t.letter-'A';
-// 			int[] qs1 = b.getQuantities();		
-// 			if(qs1[i]!=qs[i]-1)
-// 				System.out.println("problem with getRand (-1)");
+ 		for(int k=0;k<9;k++) {
+ 			int[] qs = b.getQuantities(); 
+ 			Tile t = b.getRand();
+ 			int i=t.letter-'A';
+ 			int[] qs1 = b.getQuantities();		
+ 			if(qs1[i]!=qs[i]-1)
+ 				System.out.println("problem with getRand (-1)");
 			
-// 			b.put(t);
-// 			b.put(t);
-// 			b.put(t);
+ 			b.put(t);
+ 			b.put(t);
+			b.put(t);
 			
-// 			if(b.getQuantities()[i]!=qs[i])
-// 				System.out.println("problem with put (-1)");
-// 		}
+			if(b.getQuantities()[i]!=qs[i])
+ 				System.out.println("problem with put (-1)");
+ 		}
 		
-// 		if(b.getTile('a')!=null || b.getTile('$')!=null || b.getTile('A')==null)
-// 			System.out.println("your getTile is wrong (-2)");
+ 		if(b.getTile('a')!=null || b.getTile('$')!=null || b.getTile('A')==null)
+ 			System.out.println("your getTile is wrong (-2)");
 		
-// 	}
+ 	}
 	
 	
 	
-// 	private static Tile[] get(String s) {
-// 		Tile[] ts=new Tile[s.length()];
-// 		int i=0;
-// 		for(char c: s.toCharArray()) {
-// 			ts[i]=Bag.getBag().getTile(c);
-// 			i++;
-// 		}
-// 		return ts;
-// 	}
+ 	private static Tile[] get(String s) {
+		Tile[] ts=new Tile[s.length()];
+ 		int i=0;
+		for(char c: s.toCharArray()) {
+			ts[i]=Bag.getBag().getTile(c);
+ 			i++;
+		}
+		return ts;
+ 	}
 	
 	
 // 	public static void testBoard() {
@@ -97,7 +100,7 @@ package test;
 
  	public static void main(String[] args) 
 	{
-// 		testBag(); // 30 points
+ 		testBag(); // 30 points
 // 		testBoard(); // 70 points
 		System.out.println("done2");		
 		//String inputString = java.util.Scanner.nextLine();	
--- test/test/Word.java
+++ test/test/Word.java
@@ -1,7 +1,44 @@
 package test;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 
 public class Word {
 
-	
+	public Tile[] tiles;
+    public int col;
+    public int row;
+    public boolean vertical;
+
+    public Word (Tile[] t,int row,int col,boolean  ver){
+        this.tiles=t;
+        this.row=row;
+        this.col=col;
+        this.vertical=ver;
+    }
+    public Tile[] getTiles(){
+        return  this.tiles;
+    }
+    public int getRow(){
+        return  this.row;
+    }
+    public int getCol(){
+        return  this.col;
+    }
+    public boolean  GetVer(){
+        return  this.vertical;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Word w = (Word) obj; // Cast to Tile
+        return col == w.col && row == w.row && this.vertical==w.vertical && Arrays.equals(this.tiles, w.tiles);
+}
+     @Override
+    public int hashCode() {
+        return Objects.hash(this.tiles, this.row,this.col,this.vertical);
+    }
 }
